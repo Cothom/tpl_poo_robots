c9005aa1a0f537e8344eb4683243de9f98eeeb48
Thomas Conte <thomas.conte@ensimag.grenoble-inp.fr>
2016-11-01 23:19:33 +0100

    nada

--- src/TestAffichage.java
+++ src/TestAffichage.java
@@ -21,88 +21,86 @@ import gui.Text;
 
 public class TestAffichage {
 
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Syntaxe: java TestLecteurDonnees <nomDeFichier>");
-            System.exit(1);
-        }
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Syntaxe: java TestLecteurDonnees <nomDeFichier>");
+			System.exit(1);
+		}
 
-	/* Version initiale Sujet :
-	   On peut intercepter les exceptions ici si on le souhaite comme dans le fichier TestLecture.java et non dans creeDonnees : choix de conception a voir avec le reste du groupe. */
-	// chargement des donnees 
-	DonneesSimulation donnees = LecteurDonnees.creeDonnees(args[0]);
-	
-	int largeur = donnees.getCarte().getNbColonnes() * donnees.getCarte().getTailleCases() / 100;
-	
-	int hauteur = donnees.getCarte().getNbLignes() * donnees.getCarte().getTailleCases() / 100;
+		/* Version initiale Sujet :
+		   On peut intercepter les exceptions ici si on le souhaite comme dans le fichier TestLecture.java et non dans creeDonnees : choix de conception a voir avec le reste du groupe. */
+		// chargement des donnees 
+		DonneesSimulation donnees = LecteurDonnees.creeDonnees(args[0]);
+
+		int largeur = donnees.getCarte().getNbColonnes() * donnees.getCarte().getTailleCases() / 100;
+
+		int hauteur = donnees.getCarte().getNbLignes() * donnees.getCarte().getTailleCases() / 100;
 
 
-	// crée la fenêtre graphique dans laquelle dessiner
-        GUISimulator gui = new GUISimulator(largeur, hauteur, Color.BLACK);
+		// crée la fenêtre graphique dans laquelle dessiner
+		GUISimulator gui = new GUISimulator(largeur, hauteur, Color.BLACK);
 
-	Simulateur simulateur = new Simulateur(gui, donnees);
-    }
+		Simulateur simulateur = new Simulateur(gui, donnees);
+	}
 
 }
 
-
 class Simulateur implements Simulable {
-    /** L'interface graphique associée */
-    private GUISimulator gui;
+	/** L'interface graphique associée */
+	private GUISimulator gui;
 
-    private DonneesSimulation donnees;
+	private DonneesSimulation donnees;
 
-    public Simulateur(GUISimulator pGui, DonneesSimulation pDonnees) {
-	this.gui = pGui;
-	gui.setSimulable(this);
+	public Simulateur(GUISimulator pGui, DonneesSimulation pDonnees) {
+		this.gui = pGui;
+		gui.setSimulable(this);
 
-	this.donnees = pDonnees;
+		this.donnees = pDonnees;
 
-	dessineCarte(donnees.getCarte());
-    }
-
-    private void dessineCarte(Carte pCarte) {
-	int tailleCases = pCarte.getTailleCases() / 100; // Changement d'Echelle
-	for (int i = 0; i < pCarte.getNbLignes(); i++) {
-	    for (int j = 0; j < pCarte.getNbColonnes(); j++) {
-		dessineCase(tailleCases * j + 50, tailleCases * i + 50, pCarte.getCase(i,j), tailleCases);
-	    }
+		dessineCarte(donnees.getCarte());
 	}
-    }
 
-    private void dessineCase(int x, int y, Case pCase, int tailleCase) {
-	NatureTerrain nature = pCase.getNature();
-	Color couleur;
-	switch (nature) {
-	case EAU :
-	    couleur = Color.BLUE;
-	    break;	    
-	case ROCHE :
-	    couleur = Color.RED;
-	    break;	    
-	case FORET :
-	    couleur = Color.GREEN;
-	    break;	    
-	default :
-	    couleur = Color.WHITE;
-	    break;
+	private void dessineCarte(Carte pCarte) {
+		int tailleCases = pCarte.getTailleCases() / 100; // Changement d'Echelle
+		for (int i = 0; i < pCarte.getNbLignes(); i++) {
+			for (int j = 0; j < pCarte.getNbColonnes(); j++) {
+				dessineCase(tailleCases * j + 50, tailleCases * i + 50, pCarte.getCase(i,j), tailleCases);
+			}
+		}
 	}
-	gui.addGraphicalElement(new Rectangle(x, y, Color.BLACK, couleur, tailleCase));
-	System.out.println("("+x+","+y+")");
 
-    }
+	private void dessineCase(int x, int y, Case pCase, int tailleCase) {
+		NatureTerrain nature = pCase.getNature();
+		Color couleur;
+		switch (nature) {
+			case EAU :
+				couleur = Color.BLUE;
+				break;	    
+			case ROCHE :
+				couleur = Color.RED;
+				break;	    
+			case FORET :
+				couleur = Color.GREEN;
+				break;	    
+			case HABITAT :
+				couleur = Color.CYAN;
+			default :
+				couleur = Color.WHITE;
+				break;
+		}
+		gui.addGraphicalElement(new Rectangle(x, y, Color.BLACK, couleur, tailleCase));
+		System.out.println("("+x+","+y+")");
 
+	}
 
+	@Override
+	public void next() {
+	}
 
-    
-    @Override
-    public void next() {
-    }
-
-    @Override
-    public void restart() {
-	gui.reset();
-    }
+	@Override
+	public void restart() {
+		gui.reset();
+	}
 }
 
 
--- src/maps/Incendie.java
+++ src/maps/Incendie.java
@@ -2,36 +2,36 @@ package maps;
 
 public class Incendie {
 
-    private Case position;
-    private int intensite;
-    private int intensite_initiale;
+	private Case position;
+	private int intensite;
+	private int intensite_initiale;
 
-    public Incendie(Case pPosition, int pIntensite) {
-	if (pPosition.getLigne() < 0 || pPosition.getColonne() < 0) throw new IllegalArgumentException("Argument invalide : coordonnée négative.");
-	if (pIntensite < 0) throw new IllegalArgumentException("Argument invalide : intensité négative.");
+	public Incendie(Case pPosition, int pIntensite) {
+		if (pPosition.getLigne() < 0 || pPosition.getColonne() < 0) throw new IllegalArgumentException("Argument invalide : coordonnée négative.");
+		if (pIntensite < 0) throw new IllegalArgumentException("Argument invalide : intensité négative.");
 
-	this.position = pPosition;
-	this.intensite_initiale = pIntensite;
-	this.intensite = pIntensite;
-    }
+		this.position = pPosition;
+		this.intensite_initiale = pIntensite;
+		this.intensite = pIntensite;
+	}
 
-    public void eteindre(int nbLitres) {
-	this.intensite = (nbLitres > this.intensite) ? 0 : this.intensite - nbLitres;
-    }
+	public void eteindre(int nbLitres) {
+		this.intensite = (nbLitres > this.intensite) ? 0 : this.intensite - nbLitres;
+	}
 
-    public boolean estEteint() {
-	return (this.intensite == 0) ? true : false;
-    }
+	public boolean estEteint() {
+		return (this.intensite == 0) ? true : false;
+	}
 
-    public int getIntensite() {
-	return this.intensite;
-    }
+	public int getIntensite() {
+		return this.intensite;
+	}
 
-    public int getIntensiteInitiale() {
-	return this.intensite_initiale;
-    }
+	public int getIntensiteInitiale() {
+		return this.intensite_initiale;
+	}
 
-    public Case getPosition() {
-	return this.position;
-    }
+	public Case getPosition() {
+		return this.position;
+	}
 }
